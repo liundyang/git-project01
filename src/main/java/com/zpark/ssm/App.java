73bd2a6c65a086f249fb3c36762d733e41a98840
15947576097@163.com <15947576097@163.com>
2023-05-06 10:46:13 +0800

    Hello Git master11

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,6 +3,10 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git master");
+        System.out.println("Hello Git hotfix");
+        System.out.println("Hello Git hotfix");
+        System.out.println("Hello Git master");
+        System.out.println("Hello Git master11");
     }
 }
